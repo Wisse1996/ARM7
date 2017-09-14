51d568a5a307e83f40c1b988be3fce266a2344e7
Zwonni de Blouw <z.deblouw@live.nl>
2017-09-14 12:32:57 +0200

    created workspace

---
+++ Assignment_Two/assignement2.c
@@ -0,0 +1,13 @@
+/* ******************** Workshop 1 -- Assignement2 **************************/
+#include <stdio.h> // standard C definitions
+#include <stdbool.h> // yeah boolean in c99
+#include "MCB2300 evaluationboard.h" // hardware related functions
+
+/*** globals ***/
+// global var
+
+/*** Main code ***/
+int main(void) {
+	// init the hardware and interrupts
+	initEvaluationBoard();
+}
